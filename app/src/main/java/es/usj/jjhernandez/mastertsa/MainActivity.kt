4b32c557ce8c423bd08dfc19d08fa4d8cb08c777
Juan José Hernández Alonso <juanjo.hernandez@movicoders.com>
2025-10-30 16:55:53 +0100

    201 finished

--- app/src/main/java/es/usj/jjhernandez/mastertsa/MainActivity.kt
+++ app/src/main/java/es/usj/jjhernandez/mastertsa/MainActivity.kt
@@ -1,77 +1,41 @@
 package es.usj.jjhernandez.mastertsa
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import es.usj.jjhernandez.mastertsa.ui.theme.MasterTSATheme
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : ComponentActivity() {
-
-    var text by mutableStateOf("Click to call")
-
-    private fun call () {
-        text = "Calling..."
-    }
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MasterTSATheme {
-                CallingScreen(
-                    text,
-                    {
-                        call()
-                    }
-                )
-            }
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
-}
 
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-fun CallingScreen(text: String, onButtonClicked: () -> Unit) {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text)
-            IconButton(onClick = { onButtonClicked() }) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_android_black_24dp), // Your new icon
-                    contentDescription = "Click to call"
-                )
-            }
-        }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun CallingScreenPreview() {
-    var text by remember { mutableStateOf("Click to call") }
+    fun publish(value: String) {
+        Toast.makeText(this, value, Toast.LENGTH_SHORT).show()
+    }
 
-    MasterTSATheme {
-        CallingScreen(text, onButtonClicked = {
-            text = "Calling..."
-        })
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.first_option -> publish("First")
+            R.id.second_option -> publish("Second")
+            else -> publish("Third")
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
