7d543a87d2f1318705afd4a950a952c529231526
Juan José Hernández Alonso <juanjo.hernandez@movicoders.com>
2025-10-31 18:13:14 +0100

    403 finished

--- app/src/main/java/es/usj/jjhernandez/mastertsa/MainActivity.kt
+++ app/src/main/java/es/usj/jjhernandez/mastertsa/MainActivity.kt
@@ -1,36 +1,37 @@
 package es.usj.jjhernandez.mastertsa
 
-import android.content.ComponentName
-import android.content.Intent
-import android.content.ServiceConnection
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Build
 import android.os.Bundle
-import android.os.IBinder
+import android.os.VibrationEffect
+import android.os.Vibrator
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import es.usj.jjhernandez.mastertsa.databinding.ActivityMainBinding
-import es.usj.jjhernandez.mastertsa.services.BoundedService
+import kotlin.math.abs
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private var lastX = 0f
+    private var lastY = 0f
+    private var lastZ = 0f
+    private var deltaXMax = 0f
+    private var deltaYMax = 0f
+    private var deltaZMax = 0f
+    private var deltaX = 0f
+    private var deltaY = 0f
+    private var deltaZ = 0f
+    private var vibrateThreshold = 0f
+    lateinit var v: Vibrator
+    private var sensorManager: SensorManager? = null
+    private var accelerometer: Sensor? = null
 
     private val view by lazy {
         ActivityMainBinding.inflate(layoutInflater)
-    }
-
-    lateinit var mBoundService: BoundedService
-    internal var mServiceBound = false
-
-    private val mServiceConnection = object : ServiceConnection {
-        override fun onServiceDisconnected(name: ComponentName) {
-            mServiceBound = false
-        }
-        override fun onServiceConnected(name: ComponentName,
-                                        service: IBinder
-        ) {
-            val myBinder = service as
-                    BoundedService.BoundServiceBinder
-            mBoundService = myBinder.service
-            mServiceBound = true
-        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,41 +39,86 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(view.root)
         supportActionBar?.hide()
-        view.btnStop.setOnClickListener { stop() }
-        view.btnPrintTimestamp.setOnClickListener {
-            printTimestamp() }
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        if(sensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+            accelerometer = sensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+            sensorManager!!.registerListener(
+                this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL
+            )
+            vibrateThreshold = accelerometer!!.maximumRange / 2
+        }
+        v = this.getSystemService(Context.VIBRATOR_SERVICE) as
+                Vibrator
     }
 
-    private fun stop() {
-        if (mServiceBound) {
-            unbindService(mServiceConnection)
-            mServiceBound = false
-        }
-        val intent = Intent(
-            this@MainActivity,
-            BoundedService::class.java
+    override fun onResume() {
+        super.onResume()
+        sensorManager!!.registerListener(
+            this, accelerometer,
+            SensorManager.SENSOR_DELAY_NORMAL
         )
-        stopService(intent)
     }
-    private fun printTimestamp() {
-        if (mServiceBound) {
-            view.tv1.text = mBoundService.getTimestamp()
+    override fun onPause() {
+        super.onPause()
+        sensorManager!!.unregisterListener(this)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+    override fun onSensorChanged(event: SensorEvent) {
+        displayCleanValues()
+        displayCurrentValues()
+        displayMaxValues()
+        deltaX = abs(lastX - event.values[0])
+        deltaY = abs(lastY - event.values[1])
+        deltaZ = abs(lastZ - event.values[2])
+        if (deltaX < 2)
+            deltaX = 0f
+        if (deltaY < 2)
+            deltaY = 0f
+        if (deltaZ < 2)
+            deltaZ = 0f
+        lastX = event.values[0]
+        lastY = event.values[1]
+        lastZ = event.values[2]
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            vibrate()
         }
     }
 
-    override fun onStart() {
-        super.onStart()
-        val intent = Intent(this, BoundedService::class.java)
-        startService(intent)
-        bindService(intent, mServiceConnection,
-            BIND_AUTO_CREATE)
+    private fun displayCleanValues() {
+        view.currentX.text = "0.0"
+        view.currentY.text = "0.0"
+        view.currentZ.text = "0.0"
+    }
+    private fun displayCurrentValues() {
+        view.currentX.text = deltaX.toString()
+        view.currentY.text = deltaY.toString()
+        view.currentZ.text = deltaZ.toString()
     }
 
-    override fun onStop() {
-        super.onStop()
-        if (mServiceBound) {
-            unbindService(mServiceConnection)
-            mServiceBound = false
+    private fun displayMaxValues() {
+        if (deltaX > deltaXMax) {
+            deltaXMax = deltaX
+            view.maxX.text = deltaXMax.toString()
+        }
+        if (deltaY > deltaYMax) {
+            deltaYMax = deltaY
+            view.maxY.text = deltaYMax.toString()
+        }
+        if (deltaZ > deltaZMax) {
+            deltaZMax = deltaZ
+            view.maxZ.text = deltaZMax.toString()
+        }
+    }
+
+    private fun vibrate() {
+        if (deltaX > vibrateThreshold || deltaY > vibrateThreshold
+            ||
+            deltaZ > vibrateThreshold) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                v.vibrate(
+                    VibrationEffect.createPredefined(VibrationEffect.EFFECT_DOUBLE_CLICK))
+            }
         }
     }
 }
